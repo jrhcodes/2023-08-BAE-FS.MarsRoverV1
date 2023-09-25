11da3d0088dafd93dbb7a99822037a7bbe75e107
Julian <mrjrhirst@gmail.com>
2023-09-25 17:53:25 +0100

    Changed package to lower.case

--- src/test/java/lower/case/marsrover/spatialmath/VectorTest.java
+++ src/test/java/lower/case/marsrover/spatialmath/VectorTest.java
@@ -13,30 +13,30 @@ public class VectorTest {
     @ParameterizedTest
     @CsvFileSource(resources = "TestPointValues.csv", numLinesToSkip = 2)
     public void getXTest(int x, int y) {
-        assertEquals((new Vector(x, y)).getX(), x);
+        Assertions.assertEquals((new com.jrhcodes.marsrover.spatialmath.Vector(x, y)).getX(), x);
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "TestPointValues.csv", numLinesToSkip = 2)
     public void getYTest(int x, int y) {
-        assertEquals((new Vector(x, y)).getY(), y);
+        Assertions.assertEquals((new com.jrhcodes.marsrover.spatialmath.Vector(x, y)).getY(), y);
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "TestPointValues.csv", numLinesToSkip = 2)
     public void equalsTest(int x, int y) {
-        Vector vector = new Vector(x, y);
-        assertEquals(vector, new Vector(x, y));
-        assertNotEquals(vector, new Vector(x, y - 1));
-        assertNotEquals(vector, new Vector(x - 1, y));
-        assertNotEquals(vector, new Vector(x, y + 1));
-        assertNotEquals(vector, new Vector(x + 1, y));
+        com.jrhcodes.marsrover.spatialmath.Vector vector = new com.jrhcodes.marsrover.spatialmath.Vector(x, y);
+        Assertions.assertEquals(vector, new com.jrhcodes.marsrover.spatialmath.Vector(x, y));
+        Assertions.assertNotEquals(vector, new com.jrhcodes.marsrover.spatialmath.Vector(x, y - 1));
+        Assertions.assertNotEquals(vector, new com.jrhcodes.marsrover.spatialmath.Vector(x - 1, y));
+        Assertions.assertNotEquals(vector, new com.jrhcodes.marsrover.spatialmath.Vector(x, y + 1));
+        Assertions.assertNotEquals(vector, new com.jrhcodes.marsrover.spatialmath.Vector(x + 1, y));
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "TestVectorValues.csv", numLinesToSkip = 2)
     public void applyTest(int x, int y, int vx, int vy, int rx, int ry) {
-        Point point = new Vector(vx, vy).applyCopy(new Point(x, y));
+        com.jrhcodes.marsrover.spatialmath.Point point = new com.jrhcodes.marsrover.spatialmath.Vector(vx, vy).applyCopy(new com.jrhcodes.marsrover.spatialmath.Point(x, y));
         assertEquals(point.getX(), rx);
         assertEquals(point.getY(), ry);
     }
